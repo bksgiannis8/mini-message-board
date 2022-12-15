db2eff4b8180a49463d48fcab28166e729eec82e
giannis <bkgiannis8@gmail.com>
2022-12-15 16:08:58 +0200

    Change the name of the file

---
+++ public/javascripts/messages.js
@@ -0,0 +1,24 @@
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    },
+    {
+      text: "General Kenobi",
+      user: "Obi-Wan",
+      added: new Date()
+    },
+    {
+      text: "It's Johnny",
+      user: "Johhny",
+      added: new Date()
+    }
+ ];
+
+ module.exports = messages
